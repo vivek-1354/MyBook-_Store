32e4558a31f79f72b5047b95bc91ec2a70221d80
vivek-1354 <108834876+vivek-1354@users.noreply.github.com>
2023-06-08 01:07:55 +0530

    delete item from user cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -71,44 +71,11 @@ exports.postCart = (req, res, next) => {
     console.log(result)
     res.redirect('/cart')
   })
-  // let fetchCart;
-  // req.user
-  //   .getCart()
-  //   .then((cart) => {
-  //     fetchCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } });
-  //   })
-  //   .then((products) => {
-  //     let product;
-
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-  //     let newQuantity = 1;
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return fetchCart.addProduct(product, {
-  //         through: {
-  //           quantity: newQuantity,
-  //         },
-  //       });
-  //     }
-  //     return Product.findByPk(prodId)
-  //       .then((product) => {
-  //         return fetchCart.addProduct(product, {
-  //           through: { quantity: newQuantity },
-  //         });
-  //       })
-  //       .catch((error) => console.log(error));
-  //   })
-  //   .catch((err) => console.log(err));
-  // res.redirect("/cart");
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  CartItem.destroy({ where: { productId: prodId } })
+  req.user.deleteItemFromCart(prodId)
     .then((ele) => console.log("Item deleted"))
     .catch((err) => {
       console.log(err);
