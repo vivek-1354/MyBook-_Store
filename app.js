eb96bab7cdc4a1ae247681d0c3d004310ec9827b
vivek-1354 <108834876+vivek-1354@users.noreply.github.com>
2023-06-23 23:27:39 +0530

    Connecting to mongodb database using mongoose

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
 
-const mongoConnect = require('./util/database').mongoConnect
+const mongoConnect = require("./util/database").mongoConnect;
 
-const User = require('./models/user')
+const User = require("./models/user");
 
 const app = express();
 
@@ -20,21 +21,28 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-    User.findById('647e25d7d05630a9871a183d')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id)
-      next()
-    })
-    .catch(err => console.log(err))
-})
+// app.use((req, res, next) => {
+//   User.findById("647e25d7d05630a9871a183d")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-})
-
+mongoose
+  .connect(
+    "mongodb+srv://testUser:et9vEiaZldFyRHPV@cluster0.aofa9mr.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(3000);
+    console.log("Connected...")
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- util/database.js
+++ util/database.js
@@ -1,29 +1,29 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
 
-let _db; 
+// let _db; 
 
-const mongoConnect = (callback) => {
-  MongoClient.connect(
-    'mongodb+srv://testUser:et9vEiaZldFyRHPV@cluster0.aofa9mr.mongodb.net/shop?retryWrites=true&w=majority'
-  )
-    .then(client => {
-      console.log('Connected!')
-      _db = client.db()
-      // console.log(_db)
-      callback();
-    })
-    .catch((error) => {
-      console.log(error);
-      throw err;
-    });
-}
+// const mongoConnect = (callback) => {
+//   MongoClient.connect(
+//     'mongodb+srv://testUser:et9vEiaZldFyRHPV@cluster0.aofa9mr.mongodb.net/shop?retryWrites=true&w=majority'
+//   )
+//     .then(client => {
+//       console.log('Connected!')
+//       _db = client.db()
+//       // console.log(_db)
+//       callback();
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       throw err;
+//     });
+// }
 
-const getDb = () => {
-  if(_db) {
-    return _db
-  }
-  throw 'No database found'
-}
-exports.mongoConnect = mongoConnect;
-exports.getDb = getDb
+// const getDb = () => {
+//   if(_db) {
+//     return _db
+//   }
+//   throw 'No database found'
+// }
+// exports.mongoConnect = mongoConnect;
+// exports.getDb = getDb
